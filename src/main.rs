895f2b33dc37a902815e791c9437e3ce83a058e0
AndreaSillano <andreasillano@gmail.com>
2023-08-03 17:55:29 +0200

    Snn parmas

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,16 @@ mod snn;
 fn main(){
     let mut snn = SnnBuilder::new();
     snn.add_layer().add_weight([
-        [0.1, 0.2],     /* weigths from input layer to the 1st neuron */
-        [0.3, 0.4],     /* weigths from input layer to the 2nd neuron */
-        [0.5, 0.6]      /* weigths from input layer to the 3rd neuron */
+        [0.1, 0.2],
+        [0.3, 0.4],
+        [0.5, 0.6]
     ]).add_neurons([
                         LIFNeuron::new(0.3, 0.05, 0.1, 1.0, 1.0),
                         LIFNeuron::new(0.3, 0.05, 0.1, 1.0, 1.0),
                         LIFNeuron::new(0.3, 0.05, 0.1, 1.0, 1.0),
     ]).add_intra_weights([
-        [0.0, -0.25],       /* weigths from the same layer to the 1st neuron */
-        [-0.10, 0.0]        /* weigths from the same layer to the 2nd neuron */
+        [0.0, -0.25],
+        [-0.10, 0.0]
     ]);
 
     println!("Params Created!")
