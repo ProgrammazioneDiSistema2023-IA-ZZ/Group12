dc073272a33ab0e8fa94b713fc8e39c03f4527bb
AndreaSillano <andreasillano@gmail.com>
2023-08-03 12:42:12 +0200

    Add Builder

--- src/snn/mod.rs
+++ src/snn/mod.rs
@@ -1,2 +1,34 @@
+use crate::snn::neuron::Neuron;
+
 pub mod neuron;
 pub mod layer;
+mod snn_builder;
+
+#[derive(Debug, Clone)]
+pub struct SnnParams<N: Neuron+ Clone>{
+    neurons: Vec<Vec<N>>,
+    extra_weights: Vec<Vec<Vec<f64>>>,
+    intra_weights: Vec<Vec<Vec<f64>>>,
+}
+pub struct SnnBuilder<N: Neuron+ Clone>{
+    params: SnnParams<N>
+}
+
+impl <N: Neuron+ Clone> SnnBuilder<N> {
+    pub fn new()->Self{
+        Self {
+            params: SnnParams {
+                neurons: vec![],
+                extra_weights: vec![],
+                intra_weights: vec![],
+            }
+        }
+    }
+    pub fn get_params(&self) -> SnnParams<N> {
+        self.params.clone()
+    }
+
+    pub fn add_weights(){
+
+    }
+}
