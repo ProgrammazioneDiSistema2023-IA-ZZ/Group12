15c6195d4fd0008256f6dd2f6a06cf9305e5dbb3
Palatrauss00 <davide.palatroni@gmail.com>
2023-08-07 11:59:13 +0200

    Aggiunta Struct Evento

--- src/snn/mod.rs
+++ src/snn/mod.rs
@@ -1,2 +1,18 @@
 pub mod neuron; /* pubblico */
     mod snn_builder; /*privato */
+    mod layer;
+
+/**
+    Oggetto che rappresenta  il valore di output generato dalle SNN
+**/
+
+pub struct Evento {
+    ts: u64, /* istante di tempo in cui viene generato l'output */
+    spikes: Vec<u8>, /* vettore che contiene tutti gli output */
+}
+
+impl Evento {
+    pub fn new(ts: u64, spikes: Vec<u8>) -> Self{
+        Self{ ts, spikes }
+    }
+}
