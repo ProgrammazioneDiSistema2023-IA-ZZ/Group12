51ffe11c22637b3725effc83beda082df30bf3d2
lara-m2000 <lara.moresco2000@gmail.com>
2023-08-25 18:11:20 +0200

    added Processor documentation and other comments

--- src/snn/processor.rs
+++ src/snn/processor.rs
@@ -5,74 +5,83 @@ use std::thread::JoinHandle;
 use crate::snn::Evento;
 use crate::snn::layer::Layer;
 use crate::snn::neuron::Neuron;
-
+/// Struttura utilizzata per processare l'input della rete neurale
 #[derive(Debug)]
 pub struct Processor { }
 
 impl Processor {
-    /**
-        Spikes è il vettore degli input della rete che diamo in pasto alla rete per ottenere l'output.
-        - Questo metodo crea un thread per ciascun layer all'interno della rete
-        Ciascun thread processa l'input del layer precedente attraverso un canale condiviso (channel) e manderà
-        l'output calcolato al prossimo layer sempre attraverso un altro canale condiviso
-    **/
-    pub fn process_events<'a, N: Neuron+Clone+'static, S: IntoIterator<Item=&'a mut Arc<Mutex<Layer<N>>>>>(self, snn: S, spikes: Vec<Evento>) -> Vec<Evento>{
-        /** Creiamo la pool di tutti i thread **/
+
+/// Struttura per processare l'input della rete. Ritorna l'output della rete.
+/// Questa funzione crea un thread per ogni layer della rete in modo da parallelizzare i calcoli;
+/// ogni layer è in comunicazione con il layer precedente e quello successivo
+/// con *channels* per scambire Eventi rappresentanti gli impulsi
+/// # Argomenti
+/// * `snn` - rete neurale che deve processare gli impulsi
+/// * `spikes` - vettore degli Eventi in input; ogni Evento un vettore di impulsi in ingresso ad un determinato istante
+    pub fn process_events<'a, N: Neuron+Clone+'static, S: IntoIterator<Item=&'a mut Arc<Mutex<Layer<N>>>>>
+        (self, snn: S, spikes: Vec<Evento>) -> Vec<Evento>{
+        /* Creiamo la pool di tutti i thread */
          let mut threads  = Vec::<JoinHandle<()>>::new();
 
-        /** Creiamo il channel a cui dare il primo layer **/
-        /** Di questo channel ci servirà il layer_rc che sarà il receiver del primo layer, il net_input_tx serve solo
-            per mandare l'input della rete (spikes)
-        **/
+        /*  Creiamo il channel per comunicare con il primo layer.
+            net_input_tx serve per mandare l'input della rete (spikes)
+            layer_rc sarà il receiver del primo layer;
+        */
         let (net_input_tx, mut layer_rc) = channel::<Evento>();
 
-        /** Creiamo il TX di input e il receiver per ciascun layer **/
+        /* Creiamo channels tra ogni layer */
         for layer_ref in snn {
 
             let layer_ref = layer_ref.clone();
-            /** Creiamo il canale condiviso per il prossimo layer **/
-            /** layer_tx manderà l'output di questo layer al next_layer_rc, ovvero il receiver del prossimo layer **/
-            /** Nel caso non ci fosse un prossimo layer, next_layer_rc sarà il receiver che prenderà l'output della rete **/
+            /* layer_tx manderà l'output di questo layer al next_layer_rc, ovvero il receiver del prossimo layer */
             let (layer_tx, next_layer_rc) = channel::<Evento>();
 
-            /** Creiamo effettivamente il thread **/
+            /* Creiamo il thread che processa il layer */
             let thread  = thread::spawn(move|| {
-                /** Prendiamo il layer in considerazione **/
+                /* Blocchiamo il layer in considerazione */
                 let mut layer = layer_ref.lock().unwrap();
-                /** Eseguiamo il compito del layer **/
+                /* Eseguiamo il compito del layer */
                 layer.process(layer_rc, layer_tx);
             });
-
-            threads.push(thread); /* Inseriamo il thread all'interno del vettore con tutti i thread creati */
-            layer_rc = next_layer_rc; /* Aggiorniamo il layer_rc, per passarlo al prossimo layer */
+            /* Inseriamo il thread all'interno del vettore con tutti i thread creati */
+            threads.push(thread);
+            /* il layer_rc del layer successivo è aggiornato
+                con il receiver del channel aperto da questo layer*/
+            layer_rc = next_layer_rc;
 
         }
+        /* layer_rc è rimasto il Receiver del channel dell'ultimo layer*/
         let net_output_rc = layer_rc;
 
         for evento in spikes {
-            if evento.spikes.iter().all(|spike|{*spike == 0u8}){
+            /*  In condizioni normali un Evento senza impulsi (i.e. vettore di soli 0)
+                non può produrre impulsi in output e non c'è bisogno che venga processato.
+                Dal momento che la nostra rete può presentare errori che comportino
+                un funzionamento anomalo, è corretto che vengano processati tutti gli eventi */
+            /*if evento.spikes.iter().all(|spike|{*spike == 0u8}){
                 continue;
-            }
+            }*/
 
             let instant = evento.ts;
 
-            /** Mandiamo l'input al primo layer **/
+            /* Mandiamo l'input al primo layer; la precedente creazione
+                dei channels farà in modo che gli impulsi vengano propagati nella rete */
             net_input_tx.send(evento)
                 .expect(&format!("ERROR: sending spikes event at t={}", instant));
             // generiamo un messaggio di errore particolare in caso di errore **/
         }
+        /* droppando il Sender al primo layer, faremo terminare in cascata tutti i thread */
+        drop(net_input_tx);
 
-        drop(net_input_tx); /** droppiamo net_input_tx, così da far terminare tutti i thread **/
-
-        /** Vettore di eventi per l'output di uscita **/
+        /* Vettore di eventi per l'output di uscita */
         let mut spikes_output = Vec::<Evento>::new();
 
-        /** Aspettiamo che arrivi l'ultimo output per inserirlo nel vettore di uscita **/
+        /* Aspettiamo che arrivino tutti gli eventi di output dall'ultimo layer*/
         while let Ok(spike_event) = net_output_rc.recv() {
             spikes_output.push(spike_event);
         }
 
-        /** Aspettiamo che riceva tutti i thread **/
+        /* Aspettiamo che riceva tutti i thread */
         for thread in threads {
             thread.join().unwrap();
         }
