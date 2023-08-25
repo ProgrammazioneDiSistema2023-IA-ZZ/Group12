2db9591fd8e6170f56d59eb0c2ca696d4e2eaaf0
lara-m2000 <lara.moresco2000@gmail.com>
2023-08-25 16:22:27 +0200

    added Neuron and LIFNeuron documentation and other comments

--- src/models/lifneuron.rs
+++ src/models/lifneuron.rs
@@ -1,31 +1,54 @@
 use crate::snn::neuron::Neuron;
-// struttura per salvare un errore stuck-at-X
+/// Struttura che rappresenta un errore stuck-at-X su un determinato bit
 #[derive(Clone, Debug)]
 struct ErrorBit{
-    error_type:u8, //0,1
+    error_type:u8, //0/1
     position:u8,
 }
 impl ErrorBit {
+/// Ritorna un nuovo **ErrorBit*
+/// # Argomenti
+/// * `error_type` - valore a cui il bit è bloccato *(0/1)*
+/// * `position` - posizione del bit bloccato
     pub fn new(error_type: u8, position: u8) -> Self {
         Self { error_type, position }
     }
 }
+/// Struttura che rappresenta un neurone di tipo *Leaky Integrate and Fire* (**LIF**)
 #[derive(Debug)]
 pub struct LIFNeuron{
     /* campi costanti */
-    v_th: f64, /* potenziale di soglia */
-    v_rest: f64, /* potenziale di riposo */
-    v_reset: f64, /* potenziale di reset */
+    /// potenziale di soglia
+    v_th: f64,
+    /// potenziale di riposo
+    v_rest: f64,
+    ///potenziale di reset
+    v_reset: f64,
+    /// costante di tempo *tau=C·R*
     tau: f64,
-    d_t: f64, /* intervallo di tempo tra due istanti successivi */
+    /// intervallo di tempo tra due istanti successivi
+    d_t: f64,
     /*campi mutabili*/
-    v_mem: f64, /* potenziale di membrana */
-    t_s: u64, /* ultimo istante di tempo in cui ha ricevuto almeno un impulso */
+    /// potenziale di membrana
+    v_mem: f64,
+    /// ultimo istante di tempo in cui si è ricevuto un impulso
+    t_s: u64,
+    /// *eventuale* errore su un bit del potenziale di membrana
     membrane_error:Option<ErrorBit>
 }
 
 impl LIFNeuron {
-
+/// Ritorna un nuovo neurone di tipo LIF
+/// # Argomenti
+/// * `v_th` - potenziale di soglia
+/// * `v_rest` - potenziale di riposo
+/// * `v_reset` - potenziale di reset
+/// * `tau` - constante di tempo
+/// * `d_t` - intervallo di tempo tra due istanti
+/// # Valori predefiniti
+/// * `v_mem` - potenziale di membrana settato al potenziale di riposo
+/// * `t_s` - istante di tempo iniziale settato a 0
+/// * `membrane_error` - nessun errore sulla memprana (Option::None)
     pub fn new(v_th: f64, v_rest: f64, v_reset: f64, tau: f64, d_t: f64)-> Self{
         Self{
             v_th,
@@ -38,7 +61,7 @@ impl LIFNeuron {
             membrane_error:None
         }
     }
-
+    /*** getters ***/
     pub fn v_rest(&self) -> f64 {
         self.v_rest
     }
@@ -57,12 +80,16 @@ impl LIFNeuron {
     pub fn t_s(&self) -> u64 {
         self.t_s
     }
+/// Funzione per controllare la presenza di un errore stuck-at-X su un bit del
+/// potenziale di membrana ed eventualmente forzare tale bit ad X
     fn check_error(&mut self){
         if self.membrane_error.is_none(){return;}
         let error=self.membrane_error.as_ref().unwrap();
         let mask=1u64<<error.position;
         match error.error_type {
+            /* stuck-at-0 */
             0 => { self.v_mem = f64::from_bits(self.v_mem.to_bits() & !mask) },
+            /* stuck-at-1 */
             1=> { self.v_mem = f64::from_bits(self.v_mem.to_bits() | mask) },
             _=>{}
         }
@@ -70,15 +97,20 @@ impl LIFNeuron {
 }
 
 impl Neuron for LIFNeuron{
+    /* in caso di un errore stuck-at-X sul potenziale di membrana, questo errore
+        deve essere forzato prima di ciascun utilizzo del potenziale *(i.e. formula e confronto)* */
     fn update_v_mem(&mut self, t: u64, intra_weight: f64, extra_weight: f64) -> u8 {
         let weight_sum = intra_weight+extra_weight;
 
         let exponent = -(((t-self.t_s)as f64)*self.d_t)/self.tau;
+        /* controllo sull'errore su v_mem prima del suo utilizzo */
         self.check_error();
         self.v_mem = self.v_rest + (self.v_mem-self.v_rest)*exponent.exp() + weight_sum;
 
         self.t_s = t;
+        /* controllo sull'errore su v_mem prima del suo confronto con la soglia */
         self.check_error();
+        /* confronto con la soglia ed ritorno del segnale*/
         return if self.v_mem > self.v_th {
             self.v_mem = self.v_reset;
             1
@@ -122,7 +154,6 @@ impl Clone for LIFNeuron{
             v_mem: self.v_mem,
             t_s: self.t_s,
             membrane_error:self.membrane_error.clone()
-
         }
     }
 }
--- src/snn/neuron.rs
+++ src/snn/neuron.rs
@@ -1,16 +1,25 @@
 pub trait Neuron: Send{
-
+/// Funzione per calcolare il nuovo potenziale di membrana del neurone;
+/// Ritorna un segnale binario 0/1
+/// # Argomenti
+/// * `t` - Instante di tempo corrente
+/// * `intra_weight` - Somma pesata dei segnali provenienti dal layer stesso
+/// * `extra_weight` - Somma pesata dei segnali provenienti dal layer precedente
     fn update_v_mem(&mut self, t: u64, intra_weight: f64, extra_weight: f64)->u8;
-
+/// Funzione per resettare i parametri del neurone a quelli iniziali
     fn init_neuron(&mut self);
+/// Funzione per per settare un errore stuck-at-X sul potenziale di membrana
+/// # Argomenti
+/// * `error_type` - valore a cui il bit è bloccato *(0/1)*
+/// * `position` - posizione del bit bloccato
     fn set_membrane_error(&mut self, error_type:u8, position:u8);
-
+/// Ritorna il valore del potenziale di soglia
     fn get_th(&self) -> f64;
-
+/// Setta il valore del potenziale di soglia
     fn set_th(&mut self, new_th: f64);
-
+/// Ritorna il valore del potenziale di membrana
     fn get_mem(&self) -> f64;
-
+/// Setta il valore del potenziale di membrana
     fn set_mem(&mut self, new_mem: f64);
 
 }
